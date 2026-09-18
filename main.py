34a04a607032153191d2fa209e5213a3f9b53385
FareehaAfsheen <fareehaafsheenchowdhury@gmail.com>
2026-09-18 15:54:09 +0600

    final commit

--- main.py
+++ main.py
@@ -27,3 +27,10 @@ class HybridDog(dog, pet):
         print("Color:", self.colour)
         print("Owner:", self.owner)
         print("Age:", self.age)
+Dog_name  = input("Enter dog name: ")
+Dog_color = input("Enter dog colour: ")
+Dog_owner = input("Enter owner name: ")
+Dog_age   = input("Enter dog age: ")
+
+h1 = HybridDog(Dog_name, Dog_color, Dog_owner, Dog_age)
+h1.show()  
