692ae90c4de498910b41f9a765b2297abd56dd33
FareehaAfsheen <fareehaafsheenchowdhury@gmail.com>
2026-09-18 15:50:12 +0600

    new class

--- main.py
+++ main.py
@@ -16,4 +16,8 @@ class pet:
     def __init__(self, o):
         self.owner = o
 
-
+class HybridDog(dog, pet):
+    def __init__(self, n, c, o, a):
+        dog.__init__(self, n, c)
+        pet.__init__(self, o)
+        self.age = a
